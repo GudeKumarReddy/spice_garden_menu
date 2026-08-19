3ff5183c041112ade911daf761277ef7329177e0
Kumar Reddy <103547881+GudeKumarReddy@users.noreply.github.com>
2026-08-19 22:13:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Paste the Apps Script Web App URL here after deployment.
-const API_URL = "PASTE_SPICE_GARDEN_APPS_SCRIPT_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbw3naGjDFRa3YRNgdbRKkuTn2V2SH6_Z5aIStoC9kJIpan3QP0Ig-O04LDVQIPY3gbV7g/exec";
